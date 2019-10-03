e851656343417c6ff35887e6b408b4dcf269812b
cyrus <cyrus@cyrus-latitude-e7470>
2019-10-03 12:44:38 +0800

    add image/svg+xml mime type.

--- share/howto-nmcli-connect-wifi.sh
+++ share/howto-nmcli-connect-wifi.sh
@@ -1,6 +1,6 @@
 # linux example
 nmcli c up id 'TP-laoyanzi-5G'
 # mac example
-networksetup -setairportnetwork en0 'TP-laoyanzi-5G' 'tplink?no!' 
+#networksetup -setairportnetwork en0 'TP-laoyanzi-5G' 'tplink?no!' 
 
 
